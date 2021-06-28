51203c5fd7b08c8a1dc81ab59aac3da5a2aa4c43
jcreate93 <chambj22@ferris.edu>
2021-06-28 12:29:15 -0400

    added notes on function of classes

--- src/EWalletApp.java
+++ src/EWalletApp.java
@@ -1,7 +1,15 @@
+/**This class is used for all GUI components, the login, and the main screen(list of options user can choose from).
+ * @author SENG 210 - Joseph Castro, Julie Chambers, Cameron Basham
+ */
+
 import java.util.ArrayList;
+/*Teacher notes: 
+ * this is the app class, has the GUI and create one object of your expense 
+ * calculator class. The expense calculator class is the implementation of the Expenser interface 
+ */
 
 public class EWalletApp {
-	//this is the app class, has the GUI and create one object of your expense calculator class. The expense calculator class is the implementation of the Expenser interface 
+	
 	private ArrayList<User> AllData;
 	public void CreateUser(String username, String password) {}
 
--- src/Expense.java
+++ src/Expense.java
@@ -1,7 +1,14 @@
+/**This class allows a user to enter an expense, its source, and designate how often the cost occurs(options include monthly,biweekly or yearly)
+ * 
+ * @author SENG 210 - Joseph Castro, Julie Chambers, Cameron Basham
+ *
+ */
+import java.util.Scanner;
 
 public class Expense {
 	String source;
 	double amount;
 	int yearlyfrequency; //1 for 1 time or once a year, 12 for monthly or or 24 for biweekly
-	//should add contructor(s)
+
+	
 }
