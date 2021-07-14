36330df4a64e858d4a6103d326d6ee0ed65e3135
jcreate93 <chambj22@ferris.edu>
2021-07-13 21:35:48 -0400

    update EWalletApp, LoginFrame, MenuFrame

--- src/LoginFrame.java
+++ src/LoginFrame.java
@@ -29,16 +29,17 @@ import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.JFrame;
 
 public class LoginFrame extends JFrame implements ActionListener {
+
+	// initialize GUI components
+	JButton nextButton;
 	
-		
-		
 	// initialize variables
 	private String userName;
 	private String password;
 	private JTextField userNameField;
 	private JTextField passwordField;
-	
-	//boolean variables
+
+	// boolean variables
 	boolean foundDigit = false;
 	boolean foundUpperCase = false;
 	boolean foundLowerCase = false;
@@ -49,12 +50,15 @@ public class LoginFrame extends JFrame implements ActionListener {
 	public String getUserName() {
 		return userName;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public JTextField getUserNameField() {
 		return userNameField;
 	}
+
 	public JTextField getPasswordField() {
 		return passwordField;
 	}
@@ -63,144 +67,140 @@ public class LoginFrame extends JFrame implements ActionListener {
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public void setUserNameField(JTextField userNameField) {
 		this.userNameField = userNameField;
-		 
+
 	}
+
 	public void setPasswordField(JTextField passwordField) {
 		this.passwordField = passwordField;
 	}
 
- LoginFrame(){	
-	
+	LoginFrame() {
 
-	
-	//initialize GUI components
-	JButton nextButton;
-	
-	
-	//create JFrame window
-	setTitle("Bank app");
-	this.setSize(420, 360);
-	
-	// mainPanel
-			JPanel mainPanel = new JPanel();
-			GridBagLayout gbl_mainPanel = new GridBagLayout();
-			gbl_mainPanel.columnWidths = new int[] { 93, 50, 104, 56, 0 };
-			gbl_mainPanel.rowHeights = new int[] { 19, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-			gbl_mainPanel.columnWeights = new double[] { 0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE };
-			gbl_mainPanel.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-					Double.MIN_VALUE };
-			mainPanel.setLayout(gbl_mainPanel);
-			getContentPane().add(mainPanel);
 
-			// welcomeLabel
-			JLabel welcomeLabel = new JLabel("Welcome to Bank App!");
-			welcomeLabel.setHorizontalAlignment(SwingConstants.TRAILING);
-			GridBagConstraints gbc_welcomeLabel = new GridBagConstraints();
-			gbc_welcomeLabel.gridwidth = 2;
-			gbc_welcomeLabel.insets = new Insets(0, 0, 5, 5);
-			gbc_welcomeLabel.gridx = 1;
-			gbc_welcomeLabel.gridy = 1;
-			mainPanel.add(welcomeLabel, gbc_welcomeLabel);
+		// create JFrame window
+		setTitle("Bank app");
+		this.setSize(420, 360);
 
-			// userNameLabel
-			JLabel userNameLabel = new JLabel("Username:");
-			GridBagConstraints gbc_userNameLabel = new GridBagConstraints();
-			gbc_userNameLabel.anchor = GridBagConstraints.EAST;
-			gbc_userNameLabel.insets = new Insets(0, 0, 5, 5);
-			gbc_userNameLabel.gridx = 1;
-			gbc_userNameLabel.gridy = 3;
-			mainPanel.add(userNameLabel, gbc_userNameLabel);
+		// mainPanel
+		JPanel mainPanel = new JPanel();
+		GridBagLayout gbl_mainPanel = new GridBagLayout();
+		gbl_mainPanel.columnWidths = new int[] { 93, 50, 104, 56, 0 };
+		gbl_mainPanel.rowHeights = new int[] { 19, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+		gbl_mainPanel.columnWeights = new double[] { 0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE };
+		gbl_mainPanel.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+				Double.MIN_VALUE };
+		mainPanel.setLayout(gbl_mainPanel);
+		getContentPane().add(mainPanel);
 
-			// userNameField
-			userNameField = new JTextField();
-			GridBagConstraints gbc_userNameField = new GridBagConstraints();
-			gbc_userNameField.insets = new Insets(0, 0, 5, 5);
-			gbc_userNameField.fill = GridBagConstraints.HORIZONTAL;
-			gbc_userNameField.gridx = 2;
-			gbc_userNameField.gridy = 3;
-			mainPanel.add(userNameField, gbc_userNameField);
-			userNameField.setColumns(10);
-			userName = userNameField.getText();
+		// welcomeLabel
+		JLabel welcomeLabel = new JLabel("Welcome to Bank App!");
+		welcomeLabel.setHorizontalAlignment(SwingConstants.TRAILING);
+		GridBagConstraints gbc_welcomeLabel = new GridBagConstraints();
+		gbc_welcomeLabel.gridwidth = 2;
+		gbc_welcomeLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_welcomeLabel.gridx = 1;
+		gbc_welcomeLabel.gridy = 1;
+		mainPanel.add(welcomeLabel, gbc_welcomeLabel);
 
-			// passwordLabel
-			JLabel passwordLabel = new JLabel("Password:");
-			GridBagConstraints gbc_passwordLabel = new GridBagConstraints();
-			gbc_passwordLabel.anchor = GridBagConstraints.EAST;
-			gbc_passwordLabel.insets = new Insets(0, 0, 5, 5);
-			gbc_passwordLabel.gridx = 1;
-			gbc_passwordLabel.gridy = 4;
-			mainPanel.add(passwordLabel, gbc_passwordLabel);
+		// userNameLabel
+		JLabel userNameLabel = new JLabel("Username:");
+		GridBagConstraints gbc_userNameLabel = new GridBagConstraints();
+		gbc_userNameLabel.anchor = GridBagConstraints.EAST;
+		gbc_userNameLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_userNameLabel.gridx = 1;
+		gbc_userNameLabel.gridy = 3;
+		mainPanel.add(userNameLabel, gbc_userNameLabel);
 
-			// passwordField
-			passwordField = new JTextField();
-			GridBagConstraints gbc_passwordField = new GridBagConstraints();
-			gbc_passwordField.insets = new Insets(0, 0, 5, 5);
-			gbc_passwordField.fill = GridBagConstraints.HORIZONTAL;
-			gbc_passwordField.gridx = 2;
-			gbc_passwordField.gridy = 4;
-			mainPanel.add(passwordField, gbc_passwordField);
-			passwordField.setColumns(10);
-			password = passwordField.getText();
-
-			// next button
-			nextButton = new JButton("Next");
-			nextButton.setFocusable(false);
-			nextButton.addActionListener(this);
-			GridBagConstraints gbc_nextButton = new GridBagConstraints();
-			gbc_nextButton.insets = new Insets(0, 0, 5, 5);
-			gbc_nextButton.gridx = 2;
-			gbc_nextButton.gridy = 6;
-			mainPanel.add(nextButton, gbc_nextButton);
-			this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			this.setVisible(true);
-
-	
-
-}
-
- public boolean Validate(String userName, String password) throws IOException {
-	
-	 //open file and starts verifying username and password against the text file
-			System.out.println("Opening file... "); //Opening file confidentialInfo.txt.
-		    FileInputStream fileByteStream = new FileInputStream("checkLogin.txt"); //created a new fileInputStream
-		    Scanner inFS = new Scanner(fileByteStream);
-		    
-		    
-		    System.out.println("Verifying your username and password.");
-		    while (inFS.hasNext()) {
-		 
-		    	String verifyUsername = inFS.next();
-		    	String verifyPassword = inFS.next();
-		    	 if (userName.equals(verifyUsername) && password.equals(verifyPassword)){
-		    		//create object of the second frame and hide this frame
-		    		
-		    		 return true; 
-		    	 }
-		    	 else { 
-		    		 JOptionPane.showMessageDialog(this,"Username or password not found!");
-		    	 }
-	}
-		    System.out.println("Username not found");
-			  //close checkLogin.txt file
-			    System.out.println("Closing program...");  
-			    fileByteStream.close(); 
-			    return false;
- }
-	
-	@Override
-	public void actionPerformed(ActionEvent arg0) {
-	MenuFrame mFrame = MenuFrame();
+		// userNameField
+		userNameField = new JTextField();
+		GridBagConstraints gbc_userNameField = new GridBagConstraints();
+		gbc_userNameField.insets = new Insets(0, 0, 5, 5);
+		gbc_userNameField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_userNameField.gridx = 2;
+		gbc_userNameField.gridy = 3;
+		mainPanel.add(userNameField, gbc_userNameField);
+		userNameField.setColumns(10);
 		userName = userNameField.getText();
-		System.out.println(userName);
 
+		// passwordLabel
+		JLabel passwordLabel = new JLabel("Password:");
+		GridBagConstraints gbc_passwordLabel = new GridBagConstraints();
+		gbc_passwordLabel.anchor = GridBagConstraints.EAST;
+		gbc_passwordLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_passwordLabel.gridx = 1;
+		gbc_passwordLabel.gridy = 4;
+		mainPanel.add(passwordLabel, gbc_passwordLabel);
+
+		// passwordField
+		passwordField = new JTextField();
+		GridBagConstraints gbc_passwordField = new GridBagConstraints();
+		gbc_passwordField.insets = new Insets(0, 0, 5, 5);
+		gbc_passwordField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_passwordField.gridx = 2;
+		gbc_passwordField.gridy = 4;
+		mainPanel.add(passwordField, gbc_passwordField);
+		passwordField.setColumns(10);
 		password = passwordField.getText();
-		System.out.println(password);
+
+		// next button
+		nextButton = new JButton("Next");
+		nextButton.setFocusable(false);
+		nextButton.addActionListener(this);
+		GridBagConstraints gbc_nextButton = new GridBagConstraints();
+		gbc_nextButton.insets = new Insets(0, 0, 5, 5);
+		gbc_nextButton.gridx = 2;
+		gbc_nextButton.gridy = 6;
+		mainPanel.add(nextButton, gbc_nextButton);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);
+
+	}
+
+	public boolean Validate(String userName, String password) throws IOException {
+
+		// open file and starts verifying username and password against the text file
+		System.out.println("Opening file... "); // Opening file confidentialInfo.txt.
+		FileInputStream fileByteStream = new FileInputStream("checkLogin.txt"); // created a new fileInputStream
+		Scanner inFS = new Scanner(fileByteStream);
+
+		System.out.println("Verifying your username and password.");
+		while (inFS.hasNext()) {
+
+			String verifyUsername = inFS.next();
+			String verifyPassword = inFS.next();
+			if (userName.equals(verifyUsername) && password.equals(verifyPassword)) {
+				// create object of the second frame and hide this frame
+
+				return true;
+			} else {
+				JOptionPane.showMessageDialog(this, "Username or password not found!");
+			}
+		}
+		System.out.println("Username not found");
+		// close checkLogin.txt file
+		System.out.println("Closing program...");
+		fileByteStream.close();
+		return false;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		if(event.getSource() == nextButton) {
+			MenuFrame mFrame = new MenuFrame();
+			userName = userNameField.getText();
+			System.out.println(userName);
+
+			password = passwordField.getText();
+			System.out.println(password);
+		}
+		
 	}
 
 }
-
--- src/MenuFrame.java
+++ src/MenuFrame.java
@@ -1,20 +1,105 @@
+import java.awt.GridBagLayout;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
-public class MenuFrame extends LoginFrame{
-	
-MenuFrame(){
-	
-	//initialize GUI components
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
+import javax.swing.UIManager;
+
+public class MenuFrame extends JFrame implements ActionListener {
+
+	// initialize GUI components
 	JButton nextButton;
+	JButton addIncomeButton;
+	JButton addExpenseButton; 
+	private JButton reportsButton;
 	
-	
-	//create JFrame window
-	setTitle("Bank app");
-	this.setSize(420, 360);
-	
-	// mainPanel
-	JPanel mainPanel = new JPanel();
-}
+	public MenuFrame() {
+
+		
+
+		// create JFrame window
+		setTitle("Bank app");
+		this.setSize(420, 360);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{406, 0};
+		gridBagLayout.rowHeights = new int[]{23, 0, 0, 0, 0, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		getContentPane().setLayout(gridBagLayout);
+		
+		//mainPanel
+		JPanel mainPanel = new JPanel();
+		GridBagConstraints gbc_mainPanel = new GridBagConstraints();
+		gbc_mainPanel.insets = new Insets(0, 0, 5, 0);
+		gbc_mainPanel.anchor = GridBagConstraints.NORTH;
+		gbc_mainPanel.fill = GridBagConstraints.HORIZONTAL;
+		gbc_mainPanel.gridx = 0;
+		gbc_mainPanel.gridy = 0;
+		getContentPane().add(mainPanel, gbc_mainPanel);
+		
+		//welcome label
+		JLabel welcomeLabel = new JLabel("Welcome!");
+		mainPanel.add(welcomeLabel);
+		
+		//Add income button
+		addIncomeButton = new JButton("Add income");
+		addIncomeButton.setBackground(UIManager.getColor("Button.disabledForeground"));
+		addIncomeButton.setForeground(UIManager.getColor("CheckBox.foreground"));
+		addIncomeButton.addActionListener(this);
+		GridBagConstraints gbc_addIncomeButton = new GridBagConstraints();
+		gbc_addIncomeButton.insets = new Insets(0, 0, 5, 0);
+		gbc_addIncomeButton.gridx = 0;
+		gbc_addIncomeButton.gridy = 2;
+		getContentPane().add(addIncomeButton, gbc_addIncomeButton);
+		
+		//add expense button
+		addExpenseButton = new JButton("Add Expense");
+		addExpenseButton.setBackground(UIManager.getColor("Button.darkShadow"));
+		addExpenseButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		GridBagConstraints gbc_addExpenseButton = new GridBagConstraints();
+		gbc_addExpenseButton.insets = new Insets(0, 0, 5, 0);
+		gbc_addExpenseButton.gridx = 0;
+		gbc_addExpenseButton.gridy = 3;
+		getContentPane().add(addExpenseButton, gbc_addExpenseButton);
+		
+		reportsButton = new JButton("Reports");
+		reportsButton.setBackground(UIManager.getColor("Button.disabledForeground"));
+		reportsButton.setForeground(UIManager.getColor("Button.disabledForeground"));
+		reportsButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		GridBagConstraints gbc_reportsButton = new GridBagConstraints();
+		gbc_reportsButton.gridx = 0;
+		gbc_reportsButton.gridy = 4;
+		getContentPane().add(reportsButton, gbc_reportsButton);
+		
+		
+
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		if(event.getSource() == addIncomeButton) {
+			JFrame incomeFrame = new JFrame();
+			String incomeName = JOptionPane.showInputDialog(incomeFrame,"Enter Income");
+		}
+		if(event.getSource() == addExpenseButton) {
+			JFrame expenseFrame = new JFrame();
+			String expenseName = JOptionPane.showInputDialog(expenseFrame,"Enter Expense");
+		}
+	}
 }
